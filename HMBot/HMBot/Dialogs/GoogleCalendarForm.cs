93f29bd0154f2cfe007a968aca7726c53f519f50
hyunsikkim1 <hyunsik.kim@hanmi.co.kr>
2017-07-06 15:43:50 +0900

    LUIS 관련 Commit

--- HMBot/HMBot/Dialogs/GoogleCalendarForm.cs
+++ HMBot/HMBot/Dialogs/GoogleCalendarForm.cs
@@ -24,6 +24,7 @@ namespace HMBot.Dialogs
     public class GoogleCalendarForm
     {
 
+
         // 4개의 Properties 
         // 1) 제목 
         [Describe("제목")]
@@ -31,6 +32,7 @@ namespace HMBot.Dialogs
    //     [Template(TemplateUsage.NotUnderstood, "입력하신 출발지를 조회할 수 없습니다.\n\n인천의 경우, '인천' 또는 'ICN' 형식으로 입력해주세요.")]
     //    [Template(TemplateUsage.Help, new string[] { "출발지는 공항코드, 한국어, 영어, 일본어, 중국어(간체,번체)를 지원합니다. \n\n인천의 경우 'ICN', '인천', 'Incheon', '仁川' 형식으로 입력해주세요." })]
         public string Title { get; set; }
+
 
 
         // 2) 장소
@@ -58,27 +60,6 @@ namespace HMBot.Dialogs
         public string DateTo { get; set; }
 
 
-        private async Task ResumeAfterOptionDialog(IDialogContext context, IAwaitable<object> result)
-        {
-            try
-            {
-                var message = await result;
-            }
-            catch (Exception ex)
-            {
-                await context.PostAsync($"오류가 발생했습니다. 빠른 시일안에 개선하겠습니다.: {ex.Message}");
-            }
-            finally
-            {
-                this.ShowOptions(context);
-            }
-        }
-        private void ShowOptions(IDialogContext context)
-        {
-            
-        }
-
- 
 
 
         public static IForm<GoogleCalendarForm> BuildForm()
@@ -86,7 +67,12 @@ namespace HMBot.Dialogs
             OnCompletionAsyncDelegate<GoogleCalendarForm> processFlightScheduleSearch = async (context, state) =>
             {
 
+
                 ParseUserInput(state.DateFrom);
+
+                ParseUserInput2(state.DateTo);
+
+
 
             };
 
@@ -122,16 +108,41 @@ namespace HMBot.Dialogs
                     string uri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c9d5cb47-ee4e-4e2f-b7a6-a9c0c1ee8054?subscription-key=2117fd4436f644ca94c87bdabd8ce2f3&timezoneOffset=0&verbose=true&q=" + strEscaped;
                     HttpResponseMessage msg = await client.GetAsync(uri);
 
+                if (msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<LuisResult>(jsonResponse);
 
-                    if (msg.IsSuccessStatusCode)
-                    {
-                        var jsonResponse = await msg.Content.ReadAsStringAsync();
-                        var _Data = JsonConvert.DeserializeObject<LuisResult>(jsonResponse);
-                        return _Data;
-                    }
+
+                    return _Data;
                 }
+
+            }
                 return null;
             }
+
+        public static async Task<LuisResult> ParseUserInput2(string strInput)
+        {
+            string strRet = string.Empty;
+            string strEscaped = Uri.EscapeDataString(strInput);
+
+
+            using (var client = new HttpClient())
+            {
+                string uri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/c9d5cb47-ee4e-4e2f-b7a6-a9c0c1ee8054?subscription-key=2117fd4436f644ca94c87bdabd8ce2f3&timezoneOffset=0&verbose=true&q=" + strEscaped;
+                HttpResponseMessage msg = await client.GetAsync(uri);
+
+
+                if (msg.IsSuccessStatusCode)
+                {
+                    var jsonResponse = await msg.Content.ReadAsStringAsync();
+                    var _Data = JsonConvert.DeserializeObject<LuisResult>(jsonResponse);
+
+                    return _Data;
+                }
+            }
+            return null;
+        }
 
     }
 }
