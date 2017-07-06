fb47ee05378e88f21cc24a4020919a8fd6f33bcb
hyunsikkim1 <hyunsik.kim@hanmi.co.kr>
2017-07-06 12:49:54 +0900

    오후 1차

--- HMBot/HMBot/Dialogs/GoogleCalendarForm.cs
+++ HMBot/HMBot/Dialogs/GoogleCalendarForm.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.Bot.Builder.FormFlow;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
-using Microsoft.Bot.Builder.Luis;
-
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Builder.FormFlow.Advanced;
+using Microsoft.Bot.Connector;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text.RegularExpressions;
 namespace HMBot.Dialogs
 {
     [Serializable]
@@ -38,21 +47,65 @@ namespace HMBot.Dialogs
 
 
 
-        // 3) 시작일시 
+        // 4) 종료일시 
         [Describe("종료일시")]
-        [Prompt("종료일시를 입력해주세요. \n\n(예시:2017년7월6일 오후10시)")]
+        [Prompt("종료일시를 입력해주세요. \n\n(예시:2017년7월6일 오후6시)")    ]
 
         public string DateTo { get; set; }
 
 
 
 
-        // 3) 종료일시 
-
 
         public static IForm<GoogleCalendarForm> BuildForm()
         {
+            OnCompletionAsyncDelegate<GoogleCalendarForm> processFlightScheduleSearch = async (context, state) =>
+            {
+                //await context.PostAsync($"[출발일 : {state.DateFrom}, 출발지 : {state.Departure}, 도착지 : {state.Arrival}] 스케줄 정보를 조회 중입니다. 잠시만 기다려 주세요.");
+            };
+
             return new FormBuilder<GoogleCalendarForm>()
+                .Field(nameof(DateFrom))
+                .Field(nameof(DateTo),
+                    validate: async (state, response) =>
+                    {
+                        //모든 날짜 포멧을 yyyy-MM-dd 형식으로 변환하여 전송
+                        var result = new ValidateResult { IsValid = true, Value = response };
+
+                        var departureDate = (response as string).Trim();
+                        if (Regex.IsMatch(departureDate, @"^(20)\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])$"))
+                        {
+                            result.Value = departureDate;
+                        }
+                        else if (Regex.IsMatch(departureDate, @"^(20)\d{2}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[0-1])$"))
+                        {
+                            result.Value = departureDate.Substring(0, 4) + "-" + departureDate.Substring(4, 2) + "-" + departureDate.Substring(6, 2);
+                        }
+                        else if (Regex.IsMatch(departureDate, @"^(0[1-9]|1[012])/(0[1-9]|[12][0-9]|3[0-1])$"))
+                        {
+                            result.Value = DateTime.Now.Year + "-" + departureDate.Substring(0, 2) + "-" + departureDate.Substring(3, 2);
+                        }
+                        else if (Regex.IsMatch(departureDate, @"^(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[0-1])$"))
+                        {
+                            result.Value = DateTime.Now.Year + "-" + departureDate.Substring(0, 2) + "-" + departureDate.Substring(2, 2);
+                        }
+                        else
+                        {
+                            DateTime tempDateTime;
+                            if (DateTime.TryParse(departureDate, out tempDateTime))
+                            {
+                                result.Value = tempDateTime.ToString("yyyy-MM-dd");
+                            }
+                            else
+                            {
+                                result.Feedback = "입력하신 날짜 형식을 이해 할 수 없습니다.";
+                                result.IsValid = false;
+                            }
+                        }
+                        return result;
+                    })
+                .AddRemainingFields()
+                .OnCompletion(processFlightScheduleSearch)
                 .Build();
         }
     }
